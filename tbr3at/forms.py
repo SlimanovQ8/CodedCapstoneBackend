adc636442bb47be992011560cbafb00cbdc4ab3c
heba-alenzi-93 <heba.khalaf.alenzi@gmail.com>
2022-08-27 02:35:44 +0300

    i did web functions

---
+++ tbr3at/forms.py
@@ -0,0 +1,37 @@
+
+from django import forms
+from django.contrib.auth import get_user_model
+from tbr3at import models
+
+
+User = get_user_model()
+
+# Register Form 
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username","password","first_name","last_name","email"]
+        widgets = {
+            "password": forms.PasswordInput(),
+        }
+
+
+# Login Form 
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget=forms.PasswordInput())
+
+
+
+#category form
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = models.Category
+        exclude = ["created_by"]
+
+
+
+class itemForm(forms.ModelForm):
+    class Meta:
+        model = models.Item
+        exclude = ["created_by"]
--- tbr3at/views.py
+++ tbr3at/views.py
@@ -7,6 +7,13 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Charity, Category, Annoucement, UserProfile, Item
 from .serializers import UsersListSerializer, UsersProfileListSerializer, UpdateProfileSerializer, UserCreateSerializer, CustomTokenObtainPairSerializer, GetUserProfile
 from .permissions import IsOwner
+from tbr3at import models
+from .forms import RegisterForm,LoginForm,CategoryForm,itemForm
+from django.contrib.auth import login,authenticate,logout
+from django.shortcuts import render,redirect
+from django.http import HttpRequest, HttpResponse
+
+
 
 # Create your views here.
 
@@ -36,3 +43,164 @@ class ProfileUpdateView(UpdateAPIView):
     lookup_field = 'id'
     lookup_url_kwarg = 'object_id'
     permission_classes = [IsOwner]
+
+
+
+
+
+
+
+
+
+
+##############################################################################################
+#  WEB VIWES 
+#############################################################################################
+
+# View for home page 
+def home_Page(request):
+    categories: list[models.Category] = list(models.Category.objects.all())
+
+    context = {
+        "categories": categories,
+    }
+    return render(request,"home_page.html",context)
+
+
+def register_user(request):
+    form =  RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.set_password(user.password)
+            print(user.first_name)
+            user.save()
+            print(user)
+
+            login(request,user)
+            return redirect("home")
+    context = {
+        "form":form,
+    }
+    return render (request, "register.html",context)
+
+
+
+# Login View 
+def login_user(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            print(username)
+            auth_user = authenticate(username=username, password=password)
+            login(request, auth_user)
+            return redirect("home")
+        
+    context = {
+        "form": form,
+    }
+    return render(request, "login.html", context)
+
+
+
+
+
+#logout view 
+def logout_user(request):
+    logout(request)
+    return redirect("home")
+
+
+
+#create ctegory
+def create_category(request: HttpRequest) -> HttpResponse:
+    if not request.user.is_authenticated:
+        return redirect("login")
+   
+    form = CategoryForm()
+    if request.method == "POST":
+        form = CategoryForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            categor = form.save(commit=False) 
+            categor.created_by = request.user 
+            categor.save()
+            return redirect("home")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "create_category.html", context)
+
+
+
+
+
+ # create Item
+def create_item(request: HttpRequest) -> HttpResponse:
+    if not request.user.is_authenticated:
+        return redirect("login")
+    
+    form = itemForm()
+    if request.method == "POST":
+
+        form = itemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            rec = form.save(commit=False)
+            rec.created_by = request.user
+            rec.save()
+            form.save_m2m()
+            return redirect("home")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "create_item.html", context)
+
+
+
+
+
+#category details
+def category_detail(request, CategoryID):
+    category = models.Category.objects.get(id=CategoryID)
+    context = {
+        "categories": {
+            "name": category.name,
+            "description": category.description,
+            "image": category.image,
+        }
+
+    }
+
+    return render(request, "category_detail.html", context)
+
+
+# item details
+
+def item_detail(request, ItemID):
+    Rec = models.Item.objects.get(id=ItemID)
+
+    context = {
+        "item": {
+            "name": Rec.name,
+            "description": Rec.description,
+            "image": Rec.image,
+            "condition": Rec.condition,
+            "isReserved": Rec.isReserved,
+            "category_name": Rec.category_name,
+            "created_by":Rec.created_by
+
+        }
+      
+
+    }
+
+    return render(request, "item_detail.html", context)
