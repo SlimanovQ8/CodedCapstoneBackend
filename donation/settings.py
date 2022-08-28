fcf8cab9f2b0900edc0d65003a4fb73335abe972
SlimanovQ8 <slimanovq8@gmail.com>
2022-08-28 17:07:01 +0300

    done with sign in and sign up and create users profiles and views

--- donation/settings.py
+++ donation/settings.py
@@ -46,7 +46,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
+    'crispy_forms'
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
